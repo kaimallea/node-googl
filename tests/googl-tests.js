33c7098db5810c8e58e9b2aee59167951f069555
Kai Mallea <kmallea@gmail.com>
2011-07-09 10:59:21 -0400

    setKey test

--- tests/googl-tests.js
+++ tests/googl-tests.js
@@ -8,3 +8,6 @@ googl.shorten('http://www.google.com/', function (result) {
 googl.expand('http://goo.gl/fbsS', function (result) {
     assert.equal(result.longUrl, 'http://www.google.com/');
 });
+
+googl.setKey('ABCDEFGabcdefg01234567');
+assert.equal(googl.key, 'ABCDEFGabcdefg01234567');
