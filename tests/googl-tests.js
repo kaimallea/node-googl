cf6a382062bd8b759e9477d901df8ebd7597268a
Kai Mallea <kmallea@gmail.com>
2011-07-09 10:53:05 -0400

    Simplest tests ever

---
+++ tests/googl-tests.js
@@ -0,0 +1,10 @@
+var googl = require('../lib/googl.js'),
+    assert = require('assert');
+
+googl.shorten('http://www.google.com/', function (result) {
+    assert.equal(result.id, 'http://goo.gl/fbsS');
+});
+
+googl.expand('http://goo.gl/fbsS', function (result) {
+    assert.equal(result.longUrl, 'http://www.google.com/');
+});
